55ff4a3787716be18ad408c5eed333935da2019d
runrun-akueri <69851339+runrun-akueri@users.noreply.github.com>
2020-09-29 23:06:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,112 +1,12 @@
-document.getElementById("btn").onclick = function() {
-document.getElementById('PhotoSpace').src = 'images/knot1.png';
+function test(el){
+	const hoge = el.getAttribute('src');
+	document.getElementById('PhotoSpace').src= hoge;
 }
 
-document.getElementById("btn2").onclick = function() {
-document.getElementById('PhotoSpace').src = 'images/rolex02.png'; 
+function test2(el){
+	const hoge = el.getAttribute('src');
+	document.getElementById('PhotoSpace2').src= hoge;
+	const nextElementSibling = el.nextElementSibling ;
+	const foo = nextElementSibling.getAttribute('src');
+	document.getElementById('PhotoSpace3').src= foo;
 }
-
-document.getElementById("btn3").onclick = function() {
-document.getElementById('PhotoSpace').src = 'images/omega1.png';
-}
-
-document.getElementById("btn4").onclick = function() {
-document.getElementById('PhotoSpace2').src = 'images/141601s_belt_017ribon01.png';
-document.getElementById('PhotoSpace3').src = 'images/141601s_chip_017.png';
-}
-
-document.getElementById("btn5").onclick = function() {
-document.getElementById('PhotoSpace2').src = 'images/141601s_belt_019ribon01.png';
-document.getElementById('PhotoSpace3').src = 'images/141601s_chip_019.png';
-}
-
-document.getElementById("btn6").onclick = function() {
-document.getElementById('PhotoSpace2').src = 'images/141601s_belt_027ribon01.png';
-document.getElementById('PhotoSpace3').src = 'images/141601s_chip_027.png';
-}
-
-document.getElementById("btn7").onclick = function() {
-document.getElementById('PhotoSpace2').src = 'images/141601s_belt_064ribon01.png';
-document.getElementById('PhotoSpace3').src = 'images/141601s_chip_064.png';
-}
-
-document.getElementById("btn8").onclick = function() {
-document.getElementById('PhotoSpace2').src = 'images/141601s_belt_072ribon01.png';
-document.getElementById('PhotoSpace3').src = 'images/141601s_chip_072.png';
-}
-
-document.getElementById("btn9").onclick = function() {
-document.getElementById('PhotoSpace2').src = 'images/141601s_belt_083ribon01.png';
-document.getElementById('PhotoSpace3').src = 'images/141601s_chip_083.png';
-}
-
-document.getElementById("btn10").onclick = function() {
-document.getElementById('PhotoSpace2').src = 'images/141601s_belt_084ribon01.png';
-document.getElementById('PhotoSpace3').src = 'images/141601s_chip_084.png';
-}
-
-document.getElementById("btn11").onclick = function() {
-document.getElementById('PhotoSpace2').src = 'images/141601s_belt_086ribon01.png';
-document.getElementById('PhotoSpace3').src = 'images/141601s_chip_086.png';
-}
-
-document.getElementById("btn12").onclick = function() {
-document.getElementById('PhotoSpace2').src = 'images/141601s_belt_090ribon01.png';
-document.getElementById('PhotoSpace3').src = 'images/141601s_chip_090.png';
-}
-
-document.getElementById("btn13").onclick = function() {
-document.getElementById('PhotoSpace2').src = 'images/141601s_belt_117ribon01.png';
-document.getElementById('PhotoSpace3').src = 'images/141601s_chip_117.png';
-}
-
-document.getElementById("btn14").onclick = function() {
-document.getElementById('PhotoSpace2').src = 'images/141601s_belt_165ribon01.png';
-document.getElementById('PhotoSpace3').src = 'images/141601s_chip_165.png';
-}
-
-document.getElementById("btn15").onclick = function() {
-document.getElementById('PhotoSpace2').src = 'images/141601s_belt_173ribon01.png';
-document.getElementById('PhotoSpace3').src = 'images/141601s_chip_173.png';
-}
-
-document.getElementById("btn16").onclick = function() {
-document.getElementById('PhotoSpace2').src = 'images/141601s_belt_183ribon01.png';
-document.getElementById('PhotoSpace3').src = 'images/141601s_chip_183.png';
-}
-
-document.getElementById("btn17").onclick = function() {
-document.getElementById('PhotoSpace2').src = 'images/141601s_belt_217ribon01.png';
-document.getElementById('PhotoSpace3').src = 'images/141601s_chip_217.png';
-}
-
-document.getElementById("btn18").onclick = function() {
-document.getElementById('PhotoSpace2').src = 'images/141601s_belt_321ribon01.png';
-document.getElementById('PhotoSpace3').src = 'images/141601s_chip_321.png';
-}
-
-document.getElementById("btn19").onclick = function() {
-document.getElementById('PhotoSpace2').src = 'images/141601s_belt_322ribon01.png';
-document.getElementById('PhotoSpace3').src = 'images/141601s_chip_322.png';
-}
-
-document.getElementById("btn20").onclick = function() {
-document.getElementById('PhotoSpace2').src = 'images/141601s_belt_841ribon01.png';
-document.getElementById('PhotoSpace3').src = 'images/141601s_chip_841.png';
-}
-
-document.getElementById("btn21").onclick = function() {
-document.getElementById('PhotoSpace2').src = 'images/141601s_belt_865ribon01.png';
-document.getElementById('PhotoSpace3').src = 'images/141601s_chip_865.png';
-}
-
-document.getElementById("btn22").onclick = function() {
-document.getElementById('PhotoSpace2').src = 'images/141601s_belt_875ribon01.png';
-document.getElementById('PhotoSpace3').src = 'images/141601s_chip_875.png';
-}
-
-document.getElementById("btn23").onclick = function() {
-document.getElementById('PhotoSpace2').src = 'images/141601s_belt_891ribon01.png';
-document.getElementById('PhotoSpace3').src = 'images/141601s_chip_891.png';
-}
-
