6c7b6e9bdf5728c3aa711a3cdf75e28b5da06a60
runrun-akueri <69851339+runrun-akueri@users.noreply.github.com>
2020-10-02 23:18:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function test(el){
 	const hoge = el.getAttribute('src');
 	document.getElementById('PhotoSpace').src= hoge;
+	const targetElements = document.getElementsByClassName('watch-opacity');
+	[].forEach.call(targetElements, function(elem) {
+	    elem.classList.remove('watch-opacity');
+	})
+	el.classList.add('watch-opacity');
+	
 }
 
 function test2(el){
@@ -9,4 +15,9 @@ function test2(el){
 	const nextElementSibling = el.nextElementSibling ;
 	const foo = nextElementSibling.getAttribute('src');
 	document.getElementById('PhotoSpace3').src= foo;
+	const targetElements = document.getElementsByClassName('belt-opacity');
+	[].forEach.call(targetElements, function(elem) {
+	    elem.classList.remove('belt-opacity');
+	})
+	el.classList.add('belt-opacity');
 }
